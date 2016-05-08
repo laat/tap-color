4ca1bc1f4e7d7447f02301c693077151cf6b5a56
Sigurd Fosseng <sigurd@fosseng.net>
2016-05-08 17:56:38 +0200

    lint

--- src/tap-color.js
+++ src/tap-color.js
@@ -1,10 +1,10 @@
-import chalk from 'chalk'
-import duplexer from 'duplexer2'
-import through from 'through2'
-import { LineStream } from 'byline'
+import chalk from 'chalk';
+import duplexer from 'duplexer2';
+import through from 'through2';
+import { LineStream } from 'byline';
 
-function color (line) {
-  return line
+function color(theLine) {
+  return theLine
     .replace(/^ok \d*/, (l) => chalk.green.bold(l))
     .replace(/^not ok \d*/, (l) => chalk.red.bold(l))
     .replace(/^Bail out!/, (l) => chalk.red.bold(l))
@@ -12,38 +12,38 @@ function color (line) {
     .replace(/^\s+---/, (l) => chalk.gray(l))
     .replace(/^\s+\.\.\./, (l) => chalk.gray(l))
     .replace(/(#\s*)(TODO|XXX|skipped|skip|pass|fail)?(.*)/i, (line, hash, mod, rest) => {
-      let out = []
+      const out = [];
       if (hash) {
-        out.push(chalk.gray(hash))
+        out.push(chalk.gray(hash));
       }
       if (mod) {
         if (mod.match(/skip|skipped/)) {
-          out.push(chalk.yellow(mod))
+          out.push(chalk.yellow(mod));
         } else if (mod.match(/pass/)) {
-          out.push(chalk.green(mod))
+          out.push(chalk.green(mod));
         } else if (mod.match(/fail/)) {
-          out.push(chalk.red(mod))
+          out.push(chalk.red(mod));
         } else {
-          out.push(chalk.magenta(mod))
+          out.push(chalk.magenta(mod));
         }
       }
       if (rest) {
-        out.push(chalk.gray(rest))
+        out.push(chalk.gray(rest));
       }
-      return out.join('')
-    })
+      return out.join('');
+    });
 }
 
-export default function colorStream () {
-  const lines = new LineStream()
-  const output = through()
-  let dup = duplexer(lines, output)
+export default function colorStream() {
+  const lines = new LineStream();
+  const output = through();
+  const dup = duplexer(lines, output);
 
   lines.on('data', (line) => {
     if (line.toString().match(/^not ok \d*/)) {
-      dup.failed = true
+      dup.failed = true;
     }
-    output.push(color(line.toString() + '\n'))
-  })
-  return dup
+    output.push(color(`${line.toString()}\n`));
+  });
+  return dup;
 }
